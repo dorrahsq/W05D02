98ca05ea48d437295ed1a9c427f3cc5954f28734
dorrahsq <dorrah.saleh.elq.js@tuwaiq.edu.sa>
2021-11-08 17:32:21 +0300

    task

--- app.js
+++ app.js
@@ -6,20 +6,15 @@ const PORT = 5500;
 
 const fs = require("fs");
 
-let nweM = [] 
+let nweM = [];
 
 fs.readFile("./movies.json", (err, data) => {
-    nweM = JSON.parse(data.toString());})
-
-const Movies = [
-  { id: 1, name: "friends", isFav: false, isDeleted: false },
-  { id: 2, name: "broklyn99", isFav: false, isDeleted: false },
-  { id: 3, name: "white collars", isFav: false, isDeleted: false },
-];
+  nweM = JSON.parse(data.toString());
+});
 
 //get not deleted
 app.get("/get", (req, res) => {
-  const notDeletedMov = []; //ok?? //مايحتاج اكتب عالملف لانه بس قيت 
+  const notDeletedMov = []; //ok?? //مايحتاج اكتب عالملف لانه بس قيت
   nweM.map((ele) => {
     if (ele.isDeleted === false) {
       notDeletedMov.push({ ele });
@@ -31,7 +26,7 @@ app.get("/get", (req, res) => {
 //get the fav
 app.get("/getFav", (req, res) => {
   const favMov = [];
-  Movies.map((ele) => {
+  nweM.map((ele) => {
     if (ele.isFav === true) {
       favMov.push({ ele });
     }
@@ -42,53 +37,56 @@ app.get("/getFav", (req, res) => {
 //get by id
 app.get("/get/:id", (req, res) => {
   const id = req.params.id;
-  const Movie = Movies.find((ele) => ele.id === Number(id));
+  const Movie = nweM.find((ele) => ele.id === Number(id));
   res.json(Movie);
 });
 
 //create
 app.post("/create", (req, res) => {
-    const { name } = req.body;
-  nweM.push({ id: nweM.length + 1, name: name, isFav: false, isDeleted: false }); //يضيف الاسم وباي ديفولت بيكون الفييفوريت والديليتيد فولس
-//   addToFile(nweM);
-// fs.writeFile(nweM);
-  fs.writeFile('./movies.json', JSON.stringify(nweM), (err) => {}) ; 
+  const { name } = req.body;
+  nweM.push({
+    id: nweM.length + 1,
+    name: name,
+    isFav: false,
+    isDeleted: false,
+  }); //يضيف الاسم وباي ديفولت بيكون الفييفوريت والديليتيد فولس
+  fs.writeFile("./movies.json", JSON.stringify(nweM), (err) => {});
   res.json(nweM);
 });
-
-
 
 //update on fav (fav)
 app.put("/putFav/:id", (req, res) => {
   const idd = req.params.id;
   for (let i = 0; i < nweM.length; i++) {
     if (nweM[i].id === Number(idd)) {
-        nweM[i].isFav = true;
+      nweM[i].isFav = true;
     }
   }
-  fs.writeFile('./movies.json', JSON.stringify(nweM), (err) => {}) ; 
+  fs.writeFile("./movies.json", JSON.stringify(nweM), (err) => {});
   res.json(nweM);
 });
 
 //update on fav (unfav)
-// app.put("/putUn/:id", (req, res) => {
-//   const idd = req.params.id;
-//   for (let i = 0; i < Movies.length; i++) {
-//     if (Movies[i].id === Number(idd)) {
-//       Movies[i].isFav = false;
-//     }
-//   }
-//   res.json(Movies);
-// });
+app.put("/putUn/:id", (req, res) => {
+  const idd = req.params.id;
+  for (let i = 0; i < Movies.length; i++) {
+    if (nweM[i].id === Number(idd)) {
+      nweM[i].isFav = false;
+    }
+  }
+  fs.writeFile("./movies.json", JSON.stringify(nweM), (err) => {});
+  res.json(nweM);
+});
 
 //delete
 app.delete("/delete/:id", (req, res) => {
   const iddd = req.params.id;
   for (let i = 0; i < nweM.length; i++) {
     if (nweM[i].id === Number(iddd)) {
-        nweM[i].isDeleted = true;
-    } }
-  fs.writeFile('./movies.json', JSON.stringify(nweM), (err) => {}) ; 
+      nweM[i].isDeleted = true;
+    }
+  }
+  fs.writeFile("./movies.json", JSON.stringify(nweM), (err) => {});
   res.json(nweM);
 });
 
